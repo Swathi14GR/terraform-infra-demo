24c4ea36aa2f7bb420a09c36e0886404e54437eb
Swathi14GR <goudarswathi@gmail.com>
2026-02-07 10:48:28 +0530

    prod tags

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tf-state-rg"
     storage_account_name = "tfstateprodassigmentdemo"
-    container_name       = "tfstate2"
+    container_name       = "tfstate3"
     key                  = "infra.tfstate"
   }
 }
